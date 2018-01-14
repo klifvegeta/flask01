da348cb308a446ca6b1616eb9740127597db9aec
singgih <klifvegeta@gmail.com>
2018-01-15 04:47:31 +0700

    dockerbaru

---
+++ web_app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+
+    @app.route('/')
+    def index():
+        return 'Halo flask, ini hot reload ya'
+
+
+    @app.route('/about')
+    def about():
+        return 'About me : orang tampan'
+
+    return app
