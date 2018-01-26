8bbb5ae868715a20f38dc2950872da36869cb92c
singgih <klifvegeta@gmail.com>
2018-01-26 09:52:34 +0700

    terbarupush

--- web_app/app.py
+++ web_app/app.py
@@ -1,16 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
-
+    app.config.from_pyfile('settings.py')
 
     @app.route('/')
     def index():
-        return 'Halo flask, ini hot reload ya'
+        import psycopg2
 
+        con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
+        cur = con.cursor()
+
+        cur.execute('select contents from page where id = 1')
+
+        contents = cur.fetchone() #mengembalikan tuple, (0,1,2)
+        con.close()
+
+        return render_template('index.html', TITLE='Flask-01', CONTENT=contents[0])
 
     @app.route('/about')
     def about():
-        return 'About me : orang tampan'
+        return render_template('about.html', TITLE='Flask-01')
+
+    @app.route('/testdb')
+    def testdb():
+        import psycopg2
+
+        con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
+        cur = con.cursor()
+
+        cur.execute('select * from page;')
+
+        id, title = cur.fetchone()
+        con.close()
+        return 'Output table page: {} - {}'.format(id, title)
 
     return app
